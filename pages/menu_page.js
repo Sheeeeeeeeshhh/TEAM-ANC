b3ad0fe012c89b8fdb7722a0f314f6843cf211c5
Nahz <zhancyrus24@gmail.com>
2022-12-03 00:31:33 +0800

    menu page

--- pages/menu_page.js
+++ pages/menu_page.js
@@ -2,10 +2,7 @@ import Navbar from "../components/home page/Navbar";
 import Footer from "../components/home page/Footer";
 import Content_menu from "../components/menu page/Content_menu";
 
-import { useEffect, useState } from "react";
-
 export default function ANC() {
-  const [query, setQuery] = useState("");
   return (
     <div>
       <Navbar />
